30b9e8064608508a5324b7bca162f8c9d71e8c69
osbrown1 <socknot@live.com>
2023-03-03 09:16:17 -0800

    debugging error CS1513

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test order");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+  }
+}
